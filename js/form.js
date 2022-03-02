296375c4cefc7a65007cf2a19bddb1c5ce486133
Asdel <edmiasdel@gmail.com>
2022-03-02 12:53:24 -0500

    mejorando codigo

--- js/form.js
+++ js/form.js
@@ -1,36 +1,58 @@
 var botonAdicionar = document.querySelector("#adicionar-paciente");
 
 botonAdicionar.addEventListener("click",function(event){
+    
     event.preventDefault();
 
     var form = document.querySelector("#form-adicionar");
-    
-    var nombre = form.nombre.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value
-    
+    var paciente = capturarDatosPaciente(form);
     var tabla = document.querySelector("#tabla-pacientes");
+    var pacienteTr = construirTr(paciente);
 
-    pacienteTr = document.createElement("tr");     
-    nombreTd = document.createElement("td");
-    alturaTd = document.createElement("td");
-    pesoTd = document.createElement("td");
-    gorduraTd = document.createElement("td");
-    imcTd = document.createElement("td");
-
-    nombreTd.textContent = nombre;
-    alturaTd.textContent = altura;
-    pesoTd.textContent = peso;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calcularIMC(peso,altura);
-
-    pacienteTr.appendChild(nombreTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
-
+    //ASIGNANDO EL HIJO (TD) A LA TABLA
     tabla.appendChild(pacienteTr);
 
+    form.reset();
+
 });
+
+
+function capturarDatosPaciente(form){  ////CAPTURANDO ELEMENTOS DE LA TABLA
+    
+    var paciente = {
+
+        nombre: form.nombre.value,
+        peso: form.peso.value,
+        altura: form.altura.value,
+        gordura: form.gordura.value,
+        imc:  calcularIMC(form.peso.value, form.altura.value)
+    }
+    
+    return paciente;
+}
+
+function construirTr(paciente){
+
+    //CREANDO ELEMENTOS DE TABLA (un tr y los td)
+    var pacienteTr = document.createElement("tr");
+    pacienteTr.classList.add('paciente');     
+    
+
+    //ASISGANDO HIJOS TD AL TR
+    pacienteTr.appendChild(construirTd(paciente.nombre, "info-nombre"));
+    pacienteTr.appendChild(construirTd(paciente.peso, "info-peso"));
+    pacienteTr.appendChild(construirTd(paciente.altura, "info-altura"));
+    pacienteTr.appendChild(construirTd(paciente.gordura, "info-gordura"));
+    pacienteTr.appendChild(construirTd(paciente.imc, "info-imc"));
+
+    return pacienteTr;
+}
+
+function construirTd(dato, clase){
+
+    var td = document.createElement('td');
+    td.classList.add(clase);
+    td.textContent = dato;
+
+    return td;
+}
